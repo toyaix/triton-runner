fca7cf8cb86cff15a6968359e5f6c1b8c0b3bff6
BobHuang <git@bobhuang.xyz>
2025-07-12 05:55:42 +0800

    underscore prefix for internal use

--- src/triton_ml_runner/check_utils.py
+++ src/triton_ml_runner/check_utils.py
@@ -2,7 +2,7 @@ import warnings
 import torch
 import triton
 
-metadata = {}
+_metadata = {}
 
 def colored_warning(message, category, filename, lineno, file=None, line=None):
     if file is None:
@@ -16,12 +16,12 @@ warnings.showwarning = colored_warning
 
 
 def check_kernel_name(kernel_name):
-    if metadata['name'] != kernel_name:
+    if _metadata['name'] != kernel_name:
         warnings.warn(
-            f"This kernel name {kernel_name} is different with metadata {metadata['name']}")
+            f"This kernel name {kernel_name} is different with metadata {_metadata['name']}")
 
 def check_triton_version():
-    kernel_version = metadata['triton_version']
+    kernel_version = _metadata['triton_version']
     installed_version = triton.__version__
     if kernel_version != installed_version:
         warnings.warn(
@@ -33,15 +33,15 @@ def check_triton_version():
 def check_cuda_arch(device):
     capability = torch.cuda.get_device_capability(device)
     capability = capability[0] * 10 + capability[1]
-    kernel_arch = metadata["target"]["arch"]
+    kernel_arch = _metadata["target"]["arch"]
     if kernel_arch != capability:
         warnings.warn(
             f"This kernel capability={kernel_arch} is different with device capability={capability}")
 
 
-def check_triton(kernel_name, t_metadata, device):
-    global metadata
-    metadata = t_metadata
+def check_triton(kernel_name, metadata, device):
+    global _metadata
+    _metadata = metadata
     check_kernel_name(kernel_name)
     check_triton_version()
     check_cuda_arch(device)
