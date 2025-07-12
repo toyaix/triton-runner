8b5d5c550359774706dcb4d1345ab6c868bf1ca1
BobHuang <git@bobhuang.xyz>
2025-07-13 06:44:57 +0800

    code simplify

--- src/triton_ml_runner/jit_utils.py
+++ src/triton_ml_runner/jit_utils.py
@@ -12,11 +12,7 @@ def jit_ttir_launch(file_dir, kernel_name, bound_args, signature_str, grid, opti
     ttir_path = os.path.join(file_dir, f"{kernel_name}.ttir")
     from triton_ml_runner.compile_utils import save_cubin_from_ttir
     save_cubin_from_ttir(ttir_path, options, kernel_name, file_dir)
-    metadata_path = os.path.join(file_dir, f"{kernel_name}.json")
-    cubin_path = os.path.join(file_dir, f"{kernel_name}.cubin")
-    from triton_ml_runner.cubin_utils import get_cufunction, cubin_launch
-    function = get_cufunction(metadata_path, cubin_path, f"{kernel_name}")
-    cubin_launch(function, signature_str, bound_args, grid)
+    jit_cubin_launch(file_dir, kernel_name, bound_args, signature_str, grid)
 
 
 def jit_launch(type_str, file_dir, kernel_name, bound_args, signature_str, grid, options):
