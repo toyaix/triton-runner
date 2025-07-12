05e9e0f4b1afea9d8b66b31d1838ce2d7715af32
BobHuang <git@bobhuang.xyz>
2025-07-13 06:25:55 +0800

    inherit from RunnerJITFunction

--- src/triton_ml_runner/jit.py
+++ src/triton_ml_runner/jit.py
@@ -1,6 +1,6 @@
 from triton.runtime.driver import driver
 from triton.runtime.jit import serialize_specialization_data, create_function_from_signature
-from triton.runtime.jit import KernelParam, DependenciesFinder, MockTensor
+from triton.runtime.jit import JITFunction, KernelInterface, T, KernelParam, DependenciesFinder, MockTensor
 from triton._utils import find_paths_if, get_iterable_path
 from typing import Callable, Generic, Iterable, Optional, TypeVar, Union, overload, Dict, Any, Tuple
 import inspect
@@ -10,102 +10,20 @@ import re
 import os
 import ast
 
-T = TypeVar("T")
 
+class RunnerJITFunction(JITFunction[KernelInterface[T]]):
 
-class KernelInterface(Generic[T]):
-    run: T
-
-    def __getitem__(self, grid) -> T:
-        """
-        A JIT function is launched with: fn[grid](*args, **kwargs).
-        Hence JITFunction.__getitem__ returns a callable proxy that
-        memorizes the grid.
-        """
-        return lambda *args, **kwargs: self.run(grid=grid, warmup=False, *args, **kwargs)
-        # return cast(T, functools.partial(cast(Callable, self.run), grid=grid))
-
-class JITFunction(KernelInterface[T]):
-    # Hook for inspecting compiled functions and modules
-    cache_hook = None
-    # Hook to signal that a kernel is done compiling and inspect compiled function.
-    # cache_hook will always be called before compilation and compiled_hook after.
-    compiled_hook = None
-
-    def _call_hook(
-        self,
-        key,
-        signature,
-        device,
-        constants,
-        options,
-        configs,
-        is_warmup,
-        before,
-    ):
-        hook = JITFunction.cache_hook if before else JITFunction.compiled_hook
-        if hook is None:
-            return False
-
-        name = self.fn.__name__
-        module = self.fn.__module__
-        arg_reprs = ", ".join([f"{param.name}: {ty}" for param, ty in zip(self.params, key[1])])
-        repr = f"{name}[num_warps={options.num_warps}, num_ctas={options.num_ctas}, num_stages={options.num_stages}, enable_fp_fusion={options.enable_fp_fusion}, launch_cooperative_grid={options.launch_cooperative_grid}]({arg_reprs})"
-
-        class JITFunctionInfo:
-
-            def __init__(self, module, name, jit_function):
-                self.module = module
-                self.name = name
-                self.jit_function = jit_function
-                pass
-
-        specialization_data = serialize_specialization_data(name, signature, constants, configs[0], options, key)
-
-        kwargs = {
-            'signature': signature,
-            'device': device,
-            'constants': constants,
-            'num_warps': options.num_warps,
-            'num_ctas': options.num_ctas,
-            'num_stages': options.num_stages,
-            'enable_fp_fusion': options.enable_fp_fusion,
-            'launch_cooperative_grid': options.launch_cooperative_grid,
-            'extern_libs': options.extern_libs,
-            'configs': configs,
-            'specialization_data': specialization_data,
-            'is_warmup': is_warmup,
-        }
-
-        return hook(
-            key=key,
-            repr=repr,
-            fn=JITFunctionInfo(module, name, self),
-            compile={"key": key, **kwargs},
-            is_manual_warmup=is_warmup,
-            already_compiled=False,
-        )
-
-    def add_pre_run_hook(self, hook):
-        '''
-        Add a hook that will be executed prior to the execution of run
-        function with args and kwargs passed into the kernel
-        '''
-        assert callable(hook)
-        self.pre_run_hooks.append(hook)
-
-    def create_binder(self):
-        """
-        Precompute as much as possible.
-        """
-        from triton.compiler import CompiledKernel, compile, ASTSource, make_backend
-        target = driver.active.get_current_target()
-        backend = make_backend(target)
-        self.CompiledKernel = CompiledKernel
-        self.compile = compile
-        self.ASTSource = ASTSource
-        binder = create_function_from_signature(self.signature, self.params, backend)
-        return {}, target, backend, binder
+    def runner(self, grid, bound_args, kwargs, options, sigkeys, signature_str):
+        assert grid is not None
+        if callable(grid):
+            grid = grid(bound_args)
+        for k in kwargs:
+            if k not in options.__dict__ and k not in sigkeys:
+                if k.lower() in ["cubin_dir", "ttir_dir", "ttgir_dir", "llir_dir", "ptx_dir"]:
+                    from .jit_utils import jit_launch
+                    jit_launch(k.lower(), kwargs[k], self.__name__, bound_args.values(), signature_str, grid)
+                else:
+                    raise KeyError("Keyword argument %s was specified but unrecognised" % k)
 
     def run(self, *args, grid, warmup, **kwargs):
         kwargs["debug"] = kwargs.get("debug", self.debug) or os.environ.get("TRITON_DEBUG", "0") == "1"
@@ -137,157 +55,13 @@ class JITFunction(KernelInterface[T]):
             assert "device_type" not in kwargs, "device_type option is deprecated; current target will be used"
             assert "device" not in kwargs, "device option is deprecated; current device will be used"
             assert "stream" not in kwargs, "stream option is deprecated; current stream will be used"
-            assert grid is not None
-            if callable(grid):
-                grid = grid(bound_args)
-            for k in kwargs:
-                if k not in options.__dict__ and k not in sigkeys:
-                    if k.lower() in ["cubin_dir", "ttir_dir", "ttgir_dir", "llir_dir", "ptx_dir"]:
-                        from .jit_utils import jit_launch
-                        jit_launch(k.lower(), kwargs[k], self.__name__, bound_args.values(), signature_str, grid)
-                    else:
-                        raise KeyError("Keyword argument %s was specified but unrecognised" % k)
-
-
-    def repr(self, _):
-        return self._fn_name if self._repr is None else self._repr(_)
+            self.runner(grid, bound_args, kwargs, options, sigkeys, signature_str)
 
     def __init__(self, fn, version=None, do_not_specialize=None, do_not_specialize_on_alignment=None, debug=None,
                  noinline=None, repr=None, launch_metadata=None):
-        # super().__init__(fn, version, do_not_specialize, do_not_specialize_on_alignment, debug,
-        #          noinline, repr, launch_metadata)
-        do_not_specialize = do_not_specialize if do_not_specialize else []
-        do_not_specialize_on_alignment = do_not_specialize_on_alignment if do_not_specialize_on_alignment else []
+        super().__init__(fn, version, do_not_specialize, do_not_specialize_on_alignment, debug, noinline, repr,
+                         launch_metadata)
 
-        self.fn = fn
-        self.module = fn.__module__
-        self.version = version
-        self.signature = inspect.signature(fn)
-        self.do_not_specialize = do_not_specialize
-        self.do_not_specialize_on_alignment = do_not_specialize_on_alignment
-        self.starting_line_number = inspect.getsourcelines(fn)[1]
-        self._repr = repr
-        self._fn_name = fn.__name__
-        self.launch_metadata = launch_metadata
-
-        self.params = []
-        for i, param in enumerate(self.signature.parameters.values()):
-            dns = i in do_not_specialize or param.name in do_not_specialize
-            dns_oa = i in do_not_specialize_on_alignment or param.name in do_not_specialize_on_alignment
-            self.params.append(KernelParam(i, param, dns, dns_oa))
-
-        # function source code (without decorators)
-        src = textwrap.dedent(inspect.getsource(fn))
-        src = src[re.search(r"^def\s+\w+\s*\(", src, re.MULTILINE).start():]
-        self._unsafe_update_src(src)
-        # cache of just-in-time compiled kernels
-        self.device_caches = defaultdict(self.create_binder)
-        self.hash = None
-
-        # Map of global variables used by the function and any functions it
-        # transitively calls, plus their values.  The values are collected when
-        # the function is first compiled.  Then every time we run the function,
-        # we check that the values of the globals match what's expected,
-        # otherwise we raise an error.
-        #
-        # Different functions can have different __globals__ maps, so the map
-        # key is actually (var name, id(__globals__)), and the map value is
-        # (value, __globals__).
-        self.used_global_vals: Dict[Tuple[str, int], Tuple[Any, Dict[str, Any]]] = {}
-
-        # JITFunction can be instantiated as kernel
-        # when called with a grid using __getitem__
-        self.kernel = None
-        self.debug = debug
-        self.noinline = noinline
-
-        # TODO(jlebar): Remove uses of these fields outside this file, then
-        # remove the fields here.
-        self.arg_names = [p.name for p in self.params]
-        self.constexprs = [p.num for p in self.params if p.is_constexpr]
-
-        # Hooks that will be called prior to executing "run"
-        self.pre_run_hooks = []
-
-        # reuse docs of wrapped function
-        self.__doc__ = fn.__doc__
-        self.__name__ = fn.__name__
-        self.__globals__ = fn.__globals__
-        self.__module__ = fn.__module__
-
-    @property
-    def cache_key(self):
-        # TODO : hash should be attribute of `self`
-        if self.hash is None:
-            dependencies_finder = DependenciesFinder(name=self.__name__, globals=self.__globals__, src=self.src)
-            dependencies_finder.visit(self.parse())
-            self.hash = dependencies_finder.ret + str(self.starting_line_number)
-            self.used_global_vals = dict(sorted(dependencies_finder.used_global_vals.items()))
-        return self.hash
-
-    def warmup(self, *args, grid, **kwargs):
-        return self.run(grid=grid, warmup=True, *map(MockTensor.wrap_dtype, args), **kwargs)
-
-    def preload(self, specialization_data):
-        from triton.compiler import compile, ASTSource
-        import json
-        import triton.language as tl
-        device = driver.active.get_current_device()
-        deserialized_obj = json.loads(specialization_data)
-        if deserialized_obj['name'] != self.fn.__name__:
-            raise RuntimeError(
-                f"Specialization data is for {deserialized_obj['name']} but trying to preload for {self.fn.__name__}")
-        constant_keys = map(tuple, deserialized_obj['constant_keys'])
-        constant_vals = deserialized_obj['constant_vals']
-        constants = {
-            key: tl.dtype(value) if tl.dtype.is_dtype(value) else value
-            for key, value in zip(constant_keys, constant_vals)
-        }
-        attrs_keys = map(tuple, deserialized_obj['attrs_keys'])
-        attrs_vals = deserialized_obj['attrs_vals']
-        attrs = dict(zip(attrs_keys, attrs_vals))
-        signature = dict(deserialized_obj['signature'].items())
-        src = ASTSource(self, signature, constants, attrs)
-        options = {
-            key: tuple(value) if isinstance(value, list) else value
-            for key, value in deserialized_obj['options'].items()
-        }
-        key = deserialized_obj['key']
-        kernel = compile(src, None, options)
-        self.device_caches[device][0][key] = kernel
-        return kernel
-
-    # we do not parse `src` in the constructor because
-    # the user might want to monkey-patch self.src dynamically.
-    # Our unit tests do this, for example.
-    def parse(self):
-        tree = ast.parse(self.src)
-        assert isinstance(tree, ast.Module)
-        assert len(tree.body) == 1
-        assert isinstance(tree.body[0], ast.FunctionDef)
-        return tree
-
-    def __call__(self, *args, **kwargs):
-        raise RuntimeError("Cannot call @triton.jit'd outside of the scope of a kernel")
-
-    def __setattr__(self, name, value):
-        # - when `.src` attribute is set, cache key of all callers need to be re-computed
-        if name == "src":
-            raise AttributeError(f"Cannot set attribute '{name}' directly. "
-                                 f"Use '_unsafe_update_src()' and manually clear `.hash` of all callers"
-                                 f"instead.")
-        super(JITFunction, self).__setattr__(name, value)
-
-    def _unsafe_update_src(self, new_src):
-        """
-        The only method allowed to modify src.
-        Bypasses the __setattr__ restriction by calling super().__setattr__ directly.
-        """
-        self.hash = None
-        super().__setattr__('src', new_src)
-
-    def __repr__(self):
-        return f"JITFunction({self.module}:{self.fn.__name__})"
 
 # -----------------------------------------------------------------------------
 # jit decorator
@@ -295,7 +69,7 @@ class JITFunction(KernelInterface[T]):
 
 
 @overload
-def jit(fn: T) -> JITFunction[T]:
+def jit(fn: T) -> RunnerJITFunction[T]:
     ...
 
 
@@ -309,7 +83,7 @@ def jit(
     do_not_specialize_on_alignment: Optional[Iterable[int]] = None,
     debug: Optional[bool] = None,
     noinline: Optional[bool] = None,
-) -> Callable[[T], JITFunction[T]]:
+) -> Callable[[T], RunnerJITFunction[T]]:
     ...
 
 
@@ -323,43 +97,20 @@ def jit(
     do_not_specialize_on_alignment: Optional[Iterable[int]] = None,
     debug: Optional[bool] = None,
     noinline: Optional[bool] = None,
-) -> Union[JITFunction[T], Callable[[T], JITFunction[T]]]:
-    """
-    Decorator for JIT-compiling a function using the Triton compiler.
+) -> Union[RunnerJITFunction[T], Callable[[T], RunnerJITFunction[T]]]:
 
-    :note: When a jit'd function is called, arguments are
-        implicitly converted to pointers if they have a :code:.data_ptr() method
-        and a .dtype attribute.
-
-    :note: This function will be compiled and run on the GPU. It will only have access to:
-
-           * python primitives,
-           * builtins within the triton package,
-           * arguments to this function,
-           * other jit'd functions
-
-    :param fn: the function to be jit-compiled
-    :type fn: Callable
-    """
-
-    def decorator(fn: T) -> JITFunction[T]:
+    def decorator(fn: T) -> RunnerJITFunction[T]:
         assert callable(fn)
-        if os.getenv("TRITON_INTERPRET", "0") == "1":
-            from triton.runtime.interpreter import InterpretedFunction
-            return InterpretedFunction(fn, version=version, do_not_specialize=do_not_specialize,
-                                       do_not_specialize_on_alignment=do_not_specialize_on_alignment, debug=debug,
-                                       noinline=noinline, repr=repr, launch_metadata=launch_metadata)
-        else:
-            return JITFunction(
-                fn,
-                version=version,
-                do_not_specialize=do_not_specialize,
-                do_not_specialize_on_alignment=do_not_specialize_on_alignment,
-                debug=debug,
-                noinline=noinline,
-                repr=repr,
-                launch_metadata=launch_metadata,
-            )
+        return RunnerJITFunction(
+            fn,
+            version=version,
+            do_not_specialize=do_not_specialize,
+            do_not_specialize_on_alignment=do_not_specialize_on_alignment,
+            debug=debug,
+            noinline=noinline,
+            repr=repr,
+            launch_metadata=launch_metadata,
+        )
 
     if fn is not None:
         return decorator(fn)
