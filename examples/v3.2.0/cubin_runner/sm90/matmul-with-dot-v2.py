386448ab30029b19442f444fafa92087fc1c63ba
BobHuang <git@bobhuang.xyz>
2025-07-24 07:21:03 +0800

    add sm90 examples

--- examples/v3.2.0/cubin_runner/sm90/matmul-with-dot-v2.py
+++ examples/v3.2.0/cubin_runner/sm90/matmul-with-dot-v2.py
@@ -55,7 +55,8 @@ def matmul(a, b):
         stride_cm, stride_ck,
         BLOCK_SIZE_M=128,
         BLOCK_SIZE_K=64,
-        BLOCK_SIZE_N=64
+        BLOCK_SIZE_N=64,
+        cubin_dir=triton_runner.get_file_dir(__file__)
     )
     return c
 
