5de82f60f0697b1b5423e5276e4d558ade5c9fc8
BobHuang <git@bobhuang.xyz>
2025-09-12 06:50:14 +0800

    simplify get_1d_injected_ir

--- triton_runner/debug_utils.py
+++ triton_runner/debug_utils.py
@@ -4,12 +4,9 @@ from .color_print import warning_debug_mode_ssa_and_op, warning_size_not_support
 
 def get_1d_injected_ir(ssa_value, original_line, indent, size, loc):
     return f"""{original_line}
-{indent}%debuger_0_i32 = arith.constant 0 : i32 loc(#loc1)
 {indent}%debuger_range          = tt.make_range {{end = {size} : i32, start = 0 : i32}} : tensor<{size}xi32> loc({loc})
-{indent}%debuger_store_splat    = tt.splat %debuger_0_i32 : i32 -> tensor<{size}xi32> loc({loc})
-{indent}%debuger_store_offs     = arith.addi %debuger_store_splat, %debuger_range : tensor<{size}xi32> loc({loc})
 {indent}%debuger_splat          = tt.splat %debug_tensor : !tt.ptr<f32> -> tensor<{size}x!tt.ptr<f32>> loc({loc})
-{indent}%debuger_ptr            = tt.addptr %debuger_splat, %debuger_store_offs : tensor<{size}x!tt.ptr<f32>>, tensor<{size}xi32> loc({loc})
+{indent}%debuger_ptr            = tt.addptr %debuger_splat, %debuger_range : tensor<{size}x!tt.ptr<f32>>, tensor<{size}xi32> loc({loc})
 {indent}tt.store %debuger_ptr, {ssa_value} : tensor<{size}x!tt.ptr<f32>> loc({loc})"""
 
 
