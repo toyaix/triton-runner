07bb8870539003813c173ed5eac99d85b2836689
BobHuang <git@bobhuang.xyz>
2025-09-14 04:47:39 +0800

    add attention debug

---
+++ debug_tool/ttir/06-attention/debug_out.py
@@ -0,0 +1,346 @@
+# attention from gpt-oss https://github.com/openai/gpt-oss/blob/f018fab/gpt_oss/triton/attention.py
+
+import torch
+import triton
+import triton.language as tl
+import triton_runner
+
+@triton_runner.jit
+def _attn_fwd(
+    Q,
+    K,
+    V,
+    Sinks,
+    # M,
+    Out,  #
+    sm_scale,
+    Start_q,
+    stride_qz,
+    stride_qh,
+    stride_qm,
+    stride_qk,  #
+    stride_kz,
+    stride_kh,
+    stride_kn,
+    stride_kk,  #
+    stride_vz,
+    stride_vh,
+    stride_vn,
+    stride_vk,  #
+    stride_oz,
+    stride_oh,
+    stride_om,
+    stride_ok,  #
+    Z,
+    H,
+    N_Q_CTX,
+    N_KV_CTX,
+    HEAD_DIM: tl.constexpr,  #
+    BLOCK_M: tl.constexpr,  #
+    BLOCK_N: tl.constexpr,  #
+    BANDWIDTH: tl.constexpr,
+):
+    tl.static_assert(BLOCK_N <= HEAD_DIM)
+    start_q = tl.load(Start_q).to(tl.int32)
+    start_m = tl.program_id(0)
+    off_hz = tl.program_id(1)
+    off_z = off_hz // H
+    off_h = off_hz % H
+    q_offset = off_z.to(tl.int64) * stride_qz + off_h.to(tl.int64) * stride_qh
+    k_offset = off_z.to(tl.int64) * stride_kz + off_h.to(tl.int64) * stride_kh
+    v_offset = off_z.to(tl.int64) * stride_vz + off_h.to(tl.int64) * stride_vh
+    o_offset = off_z.to(tl.int64) * stride_oz + off_h.to(tl.int64) * stride_oh
+
+    # block pointers
+    Q_block_ptr = tl.make_block_ptr(
+        base=Q + q_offset,
+        shape=(N_Q_CTX, HEAD_DIM),
+        strides=(stride_qm, stride_qk),
+        offsets=(start_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, HEAD_DIM),
+        order=(1, 0),
+    )
+    V_block_ptr = tl.make_block_ptr(
+        base=V + v_offset,
+        shape=(N_KV_CTX, HEAD_DIM),
+        strides=(stride_vn, stride_vk),
+        offsets=(0, 0),
+        block_shape=(BLOCK_N, HEAD_DIM),
+        order=(1, 0),
+    )
+    K_block_ptr = tl.make_block_ptr(
+        base=K + k_offset,
+        shape=(HEAD_DIM, N_KV_CTX),
+        strides=(stride_kk, stride_kn),
+        offsets=(0, 0),
+        block_shape=(HEAD_DIM, BLOCK_N),
+        order=(0, 1),
+    )
+
+    O_block_ptr = tl.make_block_ptr(
+        base=Out + o_offset,
+        shape=(N_Q_CTX, HEAD_DIM),
+        strides=(stride_om, stride_ok),
+        offsets=(start_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, HEAD_DIM),
+        order=(1, 0),
+    )
+
+    # load attention sinks
+    if Sinks is not None:
+        sink = tl.load(Sinks + off_h).to(tl.float32)
+    else:
+        sink = 0
+
+    # initialize offsets
+    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = tl.arange(0, BLOCK_N)
+    # initialize pointer to m and l
+    m_i = tl.zeros([BLOCK_M], dtype=tl.float32) + sink
+    l_i = tl.zeros([BLOCK_M], dtype=tl.float32)
+    acc = tl.zeros([BLOCK_M, HEAD_DIM], dtype=tl.float32)
+    # load scales
+    qk_scale = sm_scale
+    q = tl.load(Q_block_ptr)
+
+    if BANDWIDTH:
+        lo, hi = tl.maximum(start_q, start_q + start_m * BLOCK_M - BANDWIDTH), start_q + (start_m + 1) * BLOCK_M
+    else:
+        lo, hi = start_q, start_q + (start_m + 1) * BLOCK_M
+
+    # advance the KV block-pointers so they point at `lo`
+    K_block_ptr = tl.advance(K_block_ptr, (0, lo))
+    V_block_ptr = tl.advance(V_block_ptr, (lo, 0))
+
+    for start_n in range(lo, hi, BLOCK_N):
+        start_n = tl.multiple_of(start_n, BLOCK_N)
+
+        mask = (start_n + offs_n)[None, :] > (start_q + offs_m)[:, None]
+
+        if BANDWIDTH:
+            too_old = (start_n + offs_n[None, :]) < (start_q + offs_m[:, None] - BANDWIDTH + 1)
+            mask = mask | too_old
+
+        k = tl.load(K_block_ptr)
+        qk = tl.dot(q, k, allow_tf32=False)
+
+        qk = qk * qk_scale + tl.where(mask, -1.0e6, 0.0)
+        m_ij = tl.maximum(m_i, tl.max(qk, 1))
+        qk -= m_ij[:, None]
+
+        p = tl.math.exp(qk)
+        alpha = tl.math.exp(m_i - m_ij)
+        l_ij = tl.sum(p, 1)
+        acc = acc * alpha[:, None]
+
+        v = tl.load(V_block_ptr)
+        v = v.to(tl.float32)
+        acc = tl.dot(p, v, acc, allow_tf32=False)
+
+        l_i = l_i * alpha + l_ij
+        m_i = m_ij
+
+        V_block_ptr = tl.advance(V_block_ptr, (BLOCK_N, 0))
+        K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_N))
+
+    sink = tl.math.exp(sink - m_i)
+    z = l_i + sink
+    acc = acc / z[:, None]
+    # m_i += tl.math.log(l_i)
+    # m_ptrs = M + off_hz * N_Q_CTX + offs_m
+    # tl.store(m_ptrs, m_i)
+    tl.store(O_block_ptr, acc.to(Out.type.element_ty))
+
+
+def attention_ref(
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    sinks: torch.Tensor,
+    sm_scale: float = 0.125,
+    sliding_window: int | None = None,
+    start_q: torch.LongTensor = 0,
+):
+    batch_size, num_queries, num_key_value_heads, num_key_value_groups, head_dim = query.shape
+    batch_size, num_keys, num_key_value_heads, head_dim = key.shape
+
+    sinks = sinks.view(1, num_key_value_heads, num_key_value_groups, 1, 1).float()
+    key = key.unsqueeze(3)
+    value = value.unsqueeze(3)
+
+    pos_keys = torch.arange(num_keys, device=query.device)
+    pos_queries = torch.arange(num_queries, device=query.device) + start_q
+    mask = pos_keys[None, :] > pos_queries[:, None]
+    mask = mask.float().masked_fill(mask, float("-inf"))
+
+    if sliding_window:
+        too_old = pos_keys[None, :] < (pos_queries[:, None] - sliding_window + 1)
+        mask.masked_fill_(too_old, float("-inf"))
+
+    logits = torch.einsum("bqhmd,bkhmd->bhmqk", query.float(), key.float()) * sm_scale
+    logits = logits + mask[None, None, None, :, :]
+
+    logits_max = torch.max(logits, dim=-1, keepdim=True).values
+    logits_or_sinks_max = torch.maximum(sinks, logits_max)
+    sinks = torch.exp(sinks - logits_or_sinks_max)
+    unnormalized_scores = torch.exp(logits - logits_or_sinks_max)
+    normalizer = unnormalized_scores.sum(dim=-1, keepdim=True) + sinks
+    scores = unnormalized_scores / normalizer
+
+    output = torch.einsum("bhmqk,bkhmd->bqhmd", scores, value.float())
+
+    output = output.reshape(batch_size, num_queries, num_key_value_heads * num_key_value_groups * head_dim).bfloat16()
+    return output
+
+
+def torch_attn(
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    sinks: torch.Tensor,
+    sm_scale: float = 0.125,
+    sliding_window: int | None = None,
+    start_q: torch.LongTensor = 0,
+):
+    batch_size, num_queries, num_key_value_heads, num_key_value_groups, head_dim = query.shape
+    batch_size, num_keys, num_key_value_heads, head_dim = key.shape
+
+    sinks = sinks.view(1, num_key_value_heads, num_key_value_groups, 1, 1).float()
+    key = key.unsqueeze(3)
+    value = value.unsqueeze(3)
+
+    pos_keys = torch.arange(num_keys, device=query.device)
+    pos_queries = torch.arange(num_queries, device=query.device) + start_q
+    mask = pos_keys[None, :] > pos_queries[:, None]
+    mask = mask.float().masked_fill(mask, float("-inf"))
+
+    if sliding_window:
+        too_old = pos_keys[None, :] < (pos_queries[:, None] - sliding_window + 1)
+        mask.masked_fill_(too_old, float("-inf"))
+
+    logits = torch.einsum("bqhmd,bkhmd->bhmqk", query.float(), key.float()) * sm_scale
+    logits = logits + mask[None, None, None, :, :]
+
+    logits_max = torch.max(logits, dim=-1, keepdim=True).values
+    logits_or_sinks_max = torch.maximum(sinks, logits_max)
+    sinks = torch.exp(sinks - logits_or_sinks_max)
+    unnormalized_scores = torch.exp(logits - logits_or_sinks_max)
+    normalizer = unnormalized_scores.sum(dim=-1, keepdim=True) + sinks
+    scores = unnormalized_scores / normalizer
+
+    output = torch.einsum("bhmqk,bkhmd->bqhmd", scores, value.float())
+
+    output = output.reshape(batch_size, num_queries, num_key_value_heads * num_key_value_groups * head_dim).bfloat16()
+    return output
+
+
+class _attention(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, q, k, v, sinks, sm_scale, bandwidth, start_q):
+        assert len(start_q) == 1
+        # move top to avoid view
+        debug_torch = torch_attn(q, k, v, sinks, sm_scale, sliding_window, start_q)
+
+        bs, n_ctx, n_kv_heads, repeat_kv, HEAD_DIM_Q = q.shape
+        bs, n_kv_ctx, n_kv_heads, HEAD_DIM_K = k.shape
+        bs, n_kv_ctx, n_kv_heads, HEAD_DIM_V = v.shape
+        n_heads = n_kv_heads * repeat_kv
+        q = q.view(bs, n_ctx, n_heads, HEAD_DIM_Q)
+        k = k.view(bs, n_kv_ctx, n_kv_heads, HEAD_DIM_K)
+        assert HEAD_DIM_Q == HEAD_DIM_K and HEAD_DIM_K == HEAD_DIM_V
+        assert HEAD_DIM_K in {16, 32, 64, 128, 256}
+
+        q = q.transpose(1, 2).contiguous()
+        k = k.repeat_interleave(repeat_kv, dim=2).transpose(1, 2).contiguous()
+        v = v.repeat_interleave(repeat_kv, dim=2).transpose(1, 2).contiguous()
+
+        BLOCK_M = 64
+        BLOCK_N = 64
+        m_pad_size = BLOCK_M - n_ctx % BLOCK_M if n_ctx % BLOCK_M != 0 else 0
+        # pad q to multiple of its block size in the n_ctx dimension (-2)
+        q = torch.nn.functional.pad(q, (0, 0, 0, m_pad_size))
+        n_pad_size = BLOCK_N - n_kv_ctx % BLOCK_N if n_kv_ctx % BLOCK_N != 0 else 0
+        # pad k and v to multiple of their block size in the n_kv_ctx dimension
+        k = torch.nn.functional.pad(k, (0, 0, 0, n_pad_size))
+        v = torch.nn.functional.pad(v, (0, 0, 0, n_pad_size))
+
+        o = torch.empty_like(q)
+        # M = torch.empty((bs, n_heads, n_ctx + m_pad_size), device=q.device, dtype=torch.float32)
+        grid = (triton.cdiv(n_ctx, BLOCK_M), bs * n_heads, 1)
+
+
+        debug_tensor = torch.empty((BLOCK_M, HEAD_DIM_K), dtype=torch.float32, device=q.device)
+        # debug_value can be "%69"(acc / z[:, None])
+        debug_value = "%69"
+
+        _attn_fwd[grid](
+            q,
+            k,
+            v,
+            sinks,
+            # M,
+            o,
+            sm_scale,
+            start_q,
+            q.stride(0),
+            q.stride(1),
+            q.stride(2),
+            q.stride(3),  #
+            k.stride(0),
+            k.stride(1),
+            k.stride(2),
+            k.stride(3),  #
+            v.stride(0),
+            v.stride(1),
+            v.stride(2),
+            v.stride(3),  #
+            o.stride(0),
+            o.stride(1),
+            o.stride(2),
+            o.stride(3),  #
+            q.shape[0],
+            q.shape[1],  #
+            N_Q_CTX=n_ctx + m_pad_size,  #
+            N_KV_CTX=n_kv_ctx,  #
+            HEAD_DIM=HEAD_DIM_K,  #
+            BANDWIDTH=bandwidth,
+            BLOCK_M=BLOCK_M,
+            BLOCK_N=BLOCK_N,
+            num_stages=2,
+            ttir_dir=triton_runner.get_file_dir(__file__),
+            debug_tensor=debug_tensor,
+            debug_value=debug_value,
+        )
+        triton_runner.color_print.blue_print(f"debug {debug_tensor}")
+        debug_torch = debug_torch.view(bs * n_ctx, n_heads * HEAD_DIM_V)
+        max_diff = torch.max(torch.abs(debug_torch[:BLOCK_M, :HEAD_DIM_K] - debug_tensor))
+        triton_runner.color_print.yellow_print(f"The maximum difference between torch and debug is {max_diff}")
+
+        # ctx.save_for_backward(q, k, v, sinks, o, M, start_q)
+        # ctx.sm_scale = sm_scale
+        # ctx.bandwidth = bandwidth
+
+        o = o[:, :, :n_ctx, :].transpose(1, 2).contiguous()
+        o = o.view(bs, n_ctx, n_heads * HEAD_DIM_V)
+        return o
+
+
+attention = _attention.apply
+
+
+if __name__ == "__main__":
+    batch_size, num_queries, num_keys, num_key_value_heads, num_key_value_groups, head_dim, sm_scale, sliding_window, start_q = 1, 128, 128, 8, 8, 64, 0.125, None, 0
+    q = torch.randn(batch_size, num_queries, num_key_value_heads, num_key_value_groups, head_dim).bfloat16().cuda()
+    k = torch.randn(batch_size, num_keys, num_key_value_heads, head_dim).bfloat16().cuda()
+    v = torch.randn(batch_size, num_keys, num_key_value_heads, head_dim).bfloat16().cuda()
+    sinks = torch.randn(num_key_value_heads * num_key_value_groups).bfloat16().cuda()
+
+    start_q = torch.tensor([start_q], dtype=torch.int32).cuda()
+
+    triton_output = attention(q, k, v, sinks, sm_scale, sliding_window, start_q)
+    torch_output = attention_ref(q, k, v, sinks, sm_scale, sliding_window, start_q)
+
+    if torch.allclose(triton_output, torch_output, atol=1e-01, rtol=1e-02):
+        print("✅ Triton and Torch match")
+    else:
+        print("❌ Triton and Torch differ")
