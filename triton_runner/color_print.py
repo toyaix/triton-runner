e96a1fcfae7a234ce1d72ed0cd5985f36e1ab13c
BobHuang <git@bobhuang.xyz>
2025-09-15 07:43:05 +0800

    fix color print

--- triton_runner/color_print.py
+++ triton_runner/color_print.py
@@ -2,11 +2,8 @@ import termcolor
 import os
 
 
-def warning_debug_mode_grid():
-    blue_print(f"In debug mode, grid is changed to (1, 1, 1)")
-
 def warning_debug_mode_ssa_and_op(ssa, op, loc, size):
-    blue_print(f"In debug mode, ssa={ssa}, op={op}, loc={loc}, size={size}")
+    blue_print(f"[triton-runner] In debug mode, ssa={ssa}, op={op}, loc={loc}, size={size}")
 
 def blue_print(text):
     print(termcolor.colored(text, "blue"), flush=True)
@@ -15,7 +12,7 @@ def yellow_print(text):
     print(termcolor.colored(text, "yellow"), flush=True)
 
 def warning_size_not_supported(ssa, op, loc, size):
-    yellow_print(f"Warning: size={size} is not supported. And ssa={ssa}, op={op}, loc={loc}")
+    yellow_print(f"[triton-runner] Warning: size={size} is not supported. And ssa={ssa}, op={op}, loc={loc}")
 
 def print_triton_cache_dir(metadata_path, always_compile=True):
     always_compile_text = "with always_compile " if always_compile else ""
