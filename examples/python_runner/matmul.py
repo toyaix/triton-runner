5fb2b3638bcb3ccd97e67b982cb6b29d3ab75d42
BobHuang <git@bobhuang.xyz>
2025-09-17 20:00:55 +0800

    update DEVICE for v3.1.0

--- examples/python_runner/matmul.py
+++ examples/python_runner/matmul.py
@@ -3,7 +3,7 @@ import triton.language as tl
 import torch
 import triton_runner
 
-if triton.__version__ == "3.2.0":
+if triton.__version__ in ["3.2.0", "3.1.0"]:
     DEVICE = torch.cuda.current_device()
 else:
     DEVICE = triton.runtime.driver.active.get_active_torch_device()
