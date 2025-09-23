7bdfa2103f7a525c7aeb5ab2bfc6ecda59cc9ef5
BobHuang <30999153+sBobHuang@users.noreply.github.com>
2025-09-24 05:57:35 +0800

    Fix always compile (#37)
    remove always_compile

--- triton_runner/__init__.py
+++ triton_runner/__init__.py
@@ -3,9 +3,5 @@ from .color_print import yellow_print
 import os
 
 
-if os.environ.get("TRITON_ALWAYS_COMPILE", "1") == "1":
-    yellow_print("[triton-runner] TRITON_ALWAYS_COMPILE defaults to ON.\nFor production, It is recommended to disable it by running: export TRITON_ALWAYS_COMPILE=0")
-
-
 def get_file_dir(file):
     return os.path.dirname(os.path.abspath(file))
