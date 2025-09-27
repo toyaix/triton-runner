e3be38923e1dd3d254e9d040cdf8b60a23399841
BobHuang <git@bobhuang.xyz>
2025-09-27 18:42:47 +0800

    update encoding 2D

--- triton_runner/debug_utils.py
+++ triton_runner/debug_utils.py
@@ -31,7 +31,7 @@ def get_1d_injected_ir(ssa_value, original_line, indent, size, encoding, loc):
 {get_injected_ir_end(indent)}
 """
 
-def get_2d_injected_ir(ssa_value, original_line, indent, size, loc):
+def get_2d_injected_ir_without_encoding(ssa_value, original_line, indent, size, loc):
     size_0, size_1 = size.split('x')
     return f"""{get_injected_ir_begin(original_line, indent, loc)}
 {indent}  %debug_range_1        = tt.make_range {{end = {size_1} : i32, start = 0 : i32}} : tensor<{size_1}xi32> {loc}
@@ -50,6 +50,26 @@ def get_2d_injected_ir(ssa_value, original_line, indent, size, loc):
 {get_injected_ir_end(indent)}
 """
 
+def get_2d_injected_ir_with_encoding(ssa_value, original_line, indent, size, encoding, loc):
+    size_0, size_1 = size.split('x')
+    encoding = encoding[2:]
+    return f"""{get_injected_ir_begin(original_line, indent, loc)}
+{indent}  %debug_range_1        = tt.make_range {{end = {size_1} : i32, start = 0 : i32}} : tensor<{size_1}xi32, #ttg.slice<{{dim = 0, parent = {encoding}}}>> {loc}
+{indent}  %debug_expand_1       = tt.expand_dims %debug_range_1 {{axis = 0 : i32}} : tensor<{size_1}xi32, #ttg.slice<{{dim = 0, parent = {encoding}}}>> -> tensor<1x{size_1}xi32, {encoding}> {loc}
+{indent}  %debug_broadcast_1    = tt.broadcast %debug_expand_1 : tensor<1x{size_1}xi32, {encoding}> -> tensor<{size}xi32, {encoding}> {loc}
+{indent}  %debug_range_0        = tt.make_range {{end = {size_0} : i32, start = 0 : i32}} : tensor<{size_0}xi32, #ttg.slice<{{dim = 1, parent = {encoding}}}>> {loc}
+{indent}  %debug_expand_0       = tt.expand_dims %debug_range_0 {{axis = 1 : i32}} : tensor<{size_0}xi32, #ttg.slice<{{dim = 1, parent = {encoding}}}>> -> tensor<{size_0}x1xi32, {encoding}> {loc}
+{indent}  %debug_size_1_i32     = arith.constant {size_1} : i32 {loc}
+{indent}  %debug_size_0_splat   = tt.splat %debug_size_1_i32 : i32 -> tensor<{size_0}x1xi32, {encoding}> {loc}
+{indent}  %debug_size_0_off     = arith.muli %debug_expand_0, %debug_size_0_splat : tensor<{size_0}x1xi32, {encoding}> {loc}
+{indent}  %debug_broadcast_0    = tt.broadcast %debug_size_0_off : tensor<{size_0}x1xi32, {encoding}> -> tensor<{size}xi32, {encoding}> {loc}
+{indent}  %debug_range          = arith.addi %debug_broadcast_0, %debug_broadcast_1 : tensor<{size}xi32, {encoding}> {loc}
+{indent}  %debug_splat          = tt.splat %debug_tensor : !tt.ptr<f32> -> tensor<{size}x!tt.ptr<f32>, {encoding}> {loc}
+{indent}  %debug_ptr            = tt.addptr %debug_splat, %debug_range : tensor<{size}x!tt.ptr<f32>, {encoding}>, tensor<{size}xi32, {encoding}> {loc}
+{indent}  tt.store %debug_ptr, {ssa_value} : tensor<{size}x!tt.ptr<f32>, {encoding}> {loc}
+{get_injected_ir_end(indent)}
+"""
+
 
 def get_injected_ir(ssa_value, op, original_line, indent, size, encoding, loc):
     loc = f"loc({loc})"
@@ -59,7 +79,10 @@ def get_injected_ir(ssa_value, op, original_line, indent, size, encoding, loc):
         return get_1d_injected_ir(ssa_value, original_line, indent, size, encoding, loc)
     elif size.count("x") == 1:
         warning_debug_mode_ssa_and_op(ssa_value, op, loc, size, encoding)
-        return get_2d_injected_ir(ssa_value, original_line, indent, size, loc)
+        if encoding:
+            return get_2d_injected_ir_with_encoding(ssa_value, original_line, indent, size, encoding, loc)
+        else:
+            return get_2d_injected_ir_without_encoding(ssa_value, original_line, indent, size, loc)
     else:
         warning_size_not_supported(ssa_value, op, loc, size)
         return original_line
