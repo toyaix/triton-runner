a98789ceb615008e39ef24071d98e2e9c698fe64
BobHuang <git@bobhuang.xyz>
2025-10-07 19:22:26 +0800

    add more case

---
+++ examples/runner/v3.5.0/README.md
@@ -0,0 +1,14 @@
+### sm75 (T4, RTX 2080, etc.)
+```shell
+python examples/runner/v3.4.0/python/matmul.py
+
+python examples/runner/v3.4.0/ttir/matmul/matmul.py
+
+python examples/runner/v3.4.0/ttgir/sm75/matmul.py
+
+python examples/runner/v3.4.0/llir/sm75/matmul.py
+
+python examples/runner/v3.4.0/ptx/sm75/matmul.py
+
+python examples/runner/v3.4.0/cubin/sm75/matmul.py
+```
--- triton_runner/check_utils.py
+++ triton_runner/check_utils.py
@@ -1,5 +1,6 @@
 import warnings
 import triton
+from .color_print import get_project_name
 
 _metadata = {}
 
@@ -17,21 +18,21 @@ warnings.showwarning = colored_warning
 
 def check_kernel_name(kernel_name):
     if _metadata['name'] != kernel_name:
-        warnings.warn(f"This kernel name {kernel_name} is different with metadata {_metadata['name']}")
+        warnings.warn(f"{get_project_name()} This kernel name {kernel_name} is different with metadata {_metadata['name']}")
 
 
 def check_triton_version():
     kernel_version = _metadata.get('triton_version', '')
     installed_version = triton.__version__
     if kernel_version and kernel_version != installed_version:
-        warnings.warn(f"This kernel Triton v{kernel_version} is different with intstalled v{installed_version}")
-    if installed_version not in ["3.0.0", "3.1.0", "3.2.0", "3.3.0", "3.3.1", "3.4.0"]:
-        warnings.warn("This runner is only support Triton v3.4.0, v3.3.x, v3.2.0, v3.1.0 or v3.0.0.")
+        warnings.warn(f"{get_project_name()} This kernel Triton v{kernel_version} is different with intstalled v{installed_version}")
+    if installed_version not in ["3.0.0", "3.1.0", "3.2.0", "3.3.0", "3.3.1", "3.4.0", "3.5.0"]:
+        warnings.warn(f"{get_project_name()} This runner is only support Triton v3.5.0, v3.4.0, v3.3.x, v3.2.0, v3.1.0 or v3.0.0.")
 
 
 def check_cuda_arch_with_capability(kernel_arch, target_arch):
     if kernel_arch != target_arch:
-        warnings.warn(f"This kernel capability={kernel_arch} is different with device capability={target_arch}")
+        warnings.warn(f"{get_project_name()} This kernel capability={kernel_arch} is different with device capability={target_arch}")
 
 
 def check_cuda_arch(target):
