540ff1ee1aec2f1959e75aac46e913e3ac8ad64a
BobHuang <git@bobhuang.xyz>
2025-10-12 11:20:08 +0800

    has encoding must be ttgir

--- triton_runner/debug_utils.py
+++ triton_runner/debug_utils.py
@@ -2,7 +2,7 @@
 
 from .color_print import warning_debug_mode_ssa_and_op, warning_size_not_supported
 
-def get_injected_ir_begin(original_line, indent, loc, python_dump):
+def get_injected_ir_begin(original_line, indent, loc, python_dump=False):
     if_begin = "" if python_dump else f"""
 {indent}%debug_pid_x            = tt.get_program_id x : i32 {loc}
 {indent}%debug_pid_y            = tt.get_program_id y : i32 {loc}
@@ -17,9 +17,8 @@ def get_injected_ir_begin(original_line, indent, loc, python_dump):
     return f"""{original_line}\n
 {indent}// triton_runner debug start{if_begin}"""
 
-def get_injected_ir_end(indent, python_dump):
-    if_end = "" if python_dump else f"""
-{indent}}}"""
+def get_injected_ir_end(indent, python_dump=False):
+    if_end = "" if python_dump else f"\n{indent}}}"
     return f"""{if_end}{indent}// triton_runner debug end"""
 
 
@@ -59,10 +58,10 @@ def get_2d_injected_ir_without_encoding(ssa_value, original_line, indent, size, 
 {get_injected_ir_end(indent, python_dump)}
 """
 
-def get_2d_injected_ir_with_encoding(ssa_value, original_line, indent, size, encoding, loc, python_dump):
+def get_2d_injected_ir_with_encoding(ssa_value, original_line, indent, size, encoding, loc):
     size_0, size_1 = size.split('x')
     encoding = encoding[2:]
-    return f"""{get_injected_ir_begin(original_line, indent, loc, python_dump)}
+    return f"""{get_injected_ir_begin(original_line, indent, loc)}
 {indent}  %debug_range_1        = tt.make_range {{end = {size_1} : i32, start = 0 : i32}} : tensor<{size_1}xi32, #ttg.slice<{{dim = 0, parent = {encoding}}}>> {loc}
 {indent}  %debug_expand_1       = tt.expand_dims %debug_range_1 {{axis = 0 : i32}} : tensor<{size_1}xi32, #ttg.slice<{{dim = 0, parent = {encoding}}}>> -> tensor<1x{size_1}xi32, {encoding}> {loc}
 {indent}  %debug_broadcast_1    = tt.broadcast %debug_expand_1 : tensor<1x{size_1}xi32, {encoding}> -> tensor<{size}xi32, {encoding}> {loc}
@@ -76,7 +75,7 @@ def get_2d_injected_ir_with_encoding(ssa_value, original_line, indent, size, enc
 {indent}  %debug_splat          = tt.splat %debug_tensor : !tt.ptr<f32> -> tensor<{size}x!tt.ptr<f32>, {encoding}> {loc}
 {indent}  %debug_ptr            = tt.addptr %debug_splat, %debug_range : tensor<{size}x!tt.ptr<f32>, {encoding}>, tensor<{size}xi32, {encoding}> {loc}
 {indent}  tt.store %debug_ptr, {ssa_value} : tensor<{size}x!tt.ptr<f32>, {encoding}> {loc}
-{get_injected_ir_end(indent, python_dump)}
+{get_injected_ir_end(indent)}
 """
 
 
