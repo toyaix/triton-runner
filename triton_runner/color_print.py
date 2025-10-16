4763b1d4bf2e78affcfb2c2b0d471e9b4cdfcec2
BobHuang <git@bobhuang.xyz>
2025-10-17 07:54:00 +0800

    is dump not debugging

--- triton_runner/color_print.py
+++ triton_runner/color_print.py
@@ -11,9 +11,9 @@ def yellow_print(text):
 def get_project_name():
     return "[Triton Runner]"
 
-def warning_debug_mode_ssa_and_op(ssa, op, loc, size, encoding):
+def warning_dump_mode_ssa_and_op(ssa, op, loc, size, encoding):
     encoding = f" with encoding={encoding[2:]}" if encoding != "" else ""
-    blue_print(f"{get_project_name()} In debug mode, ssa={ssa}, op={op}, loc={loc}, size={size}{encoding}")
+    blue_print(f"{get_project_name()} In dump mode, ssa={ssa}, op={op}, loc={loc}, size={size}{encoding}")
 
 def warning_size_not_supported(ssa, op, loc, size):
     yellow_print(f"{get_project_name()} Warning: size={size} is not supported. And ssa={ssa}, op={op}, loc={loc}")
