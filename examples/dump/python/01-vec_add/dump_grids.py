3249f5190dfb4867c33030965994c56be7246cc8
BobHuang <git@bobhuang.xyz>
2025-10-18 22:49:28 +0800

    use 1D tensor

--- examples/dump/python/01-vec_add/dump_grids.py
+++ examples/dump/python/01-vec_add/dump_grids.py
@@ -54,8 +54,9 @@ def add(x: torch.Tensor, y: torch.Tensor):
     # In this case, we use a 1D grid where the size is the number of blocks:
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
     BLOCK_SIZE = 1024
-    new_shape = tuple(triton.cdiv(dim, BLOCK_SIZE) * BLOCK_SIZE for dim in x.shape)
-    dump_tensor = torch.empty(new_shape, dtype=torch.float32, device=x.device)
+    import math
+    new_shape = tuple(triton.cdiv(dim, block) * block for dim, block in zip(x.shape, [BLOCK_SIZE]))
+    dump_tensor = torch.empty(math.prod(new_shape), dtype=torch.float32, device=x.device)
 
     # NOTE:
     #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
