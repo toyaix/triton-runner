43114c333214321a23960aabbaff119c69514ad6
BobHuang <git@bobhuang.xyz>
2025-11-27 20:39:41 +0800

    language use triton version

--- triton_runner/language/__init__.py
+++ triton_runner/language/__init__.py
@@ -1,6 +1,7 @@
 import triton
+from ..version_utils import is_triton_geq_v3_4
 
-if triton.__version__ in ["3.4.0", "3.5.0"]:
+if is_triton_geq_v3_4:
     from .dump import dump, dump_boundary, dump_grids
 else:
     from .dump_before_3_4_0 import dump, dump_boundary, dump_grids
