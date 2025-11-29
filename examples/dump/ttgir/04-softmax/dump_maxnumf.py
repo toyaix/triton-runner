fix 04-softmax ttir/ttgir dump

examples/dump/ttgir/04-softmax/dump_maxnumf.py
@@ -36,10 +36,10 @@ def softmax_kernel(
 def solve(input: torch.Tensor, output: torch.Tensor, N: int):
     grid = lambda META: (triton.cdiv(N, META['BLOCK_SIZE']), )
 
-    BLOCK_SIZE = 32768
+    BLOCK_SIZE = 4096
     dump_tensor = torch.empty((BLOCK_SIZE), dtype=torch.float32, device=input.device)
-    # dump_value can be "%26"(_max in loop)
-    dump_value = "%26"
+    # dump_value can be "%_max_14"(_max = tl.maximum(a, _max))
+    dump_value = "%_max_14"
 
     softmax_kernel[grid](
         input, output, N,

examples/dump/ttir/04-softmax/dump_divf-normalize_by_sum.py
@@ -43,10 +43,10 @@ def torch_softmax(input):
 def solve(input: torch.Tensor, output: torch.Tensor, N: int):
     grid = lambda META: (triton.cdiv(N, META['BLOCK_SIZE']), )
 
-    BLOCK_SIZE = 32768
+    BLOCK_SIZE = 4096
     dump_tensor = torch.empty((BLOCK_SIZE), dtype=torch.float32, device=input.device)
-    # dump_value can be "%18"(normalize_by_sum)
-    dump_value = "%18"
+    # dump_value can be "%normalize_by_sum_9"(normalize_by_sum =  exp_shifted / sum)
+    dump_value = "%normalize_by_sum_9"
 
     softmax_kernel[grid](
         input, output, N,